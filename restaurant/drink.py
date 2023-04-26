19d1dfe3b273c663bf5849bb9e9d1a1dff82ad31
MDonyk <donyk.rita.testfort@gmail.com>
2023-04-26 20:11:02 +0300

    MDonyk Added restaurant task

---
+++ restaurant/drink.py
@@ -0,0 +1,17 @@
+from menu_item import MenuItem
+
+
+class Drink(MenuItem):
+    def __init__(self, name, price, description, size, is_refillable):
+        super().__init__(name, price, description, False) # Drinks are not vegetarian by default
+        self.size = size
+        self.is_refillable = is_refillable
+
+    def get_size(self):
+        return self.size
+
+    def is_refillable(self):
+        return self.is_refillable
+
+    def __str__(self):
+        return f"{self.name} - ${self.price}: {self.description} ({self.size}, Refillable: {self.is_refillable})"
