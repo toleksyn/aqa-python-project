62f820ba78af8eef7c628f4b7c68f663a781e002
RomanO-qa <romantestfort@gmail.com>
2023-04-26 13:21:05 +0300

    Added main.py file

---
+++ main.py
@@ -0,0 +1,22 @@
+# Create a class hierarchy for a restaurant ordering system
+# Create an instance of the Restaurant class and add a few orders with multiple items each.
+from Omelchenko_Homework6.Restaurant.appetizer import Appetizer
+from Omelchenko_Homework6.Restaurant.entree import Entree
+from Omelchenko_Homework6.Restaurant.drink import Drink
+from Omelchenko_Homework6.Restaurant.order import Order
+from Omelchenko_Homework6.Restaurant.restaurant import Restaurant
+
+first_order = Order(1)
+first_order.add_item(Appetizer("Canape", 2, "small sandwich with oil and vegetables", False, True))
+first_order.add_item(Entree("Soup", 5, "Chicken soup with macaronis", False, True))
+first_order.add_item(Drink("Coca-Cola", 1, "Bottle of CocaCola", 0.5, True))
+second_order = Order(2)
+second_order.add_item(Appetizer("Capreze", 5, "Salad with tomatoes and cheese", False, True))
+second_order.add_item(Entree("Steak", 30, "Medium-rare beef steak with cherry sauce", False, False))
+second_order.add_item(Drink("Coffee", 2, "A cup of espresso", 0.1, True))
+print(first_order.__str__())
+print(second_order.__str__())
+grill_bar = Restaurant("GrillBar")
+grill_bar.add_order(1, first_order)
+grill_bar.add_order(2, second_order)
+print(grill_bar.__str__())
