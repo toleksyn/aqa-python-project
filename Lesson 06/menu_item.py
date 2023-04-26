0cefe5bd0f27b2ceb427ce0e12f49b730257b406
Viktor Nesterenko <109369648+RenkoNester@users.noreply.github.com>
2023-04-26 08:20:02 +0300

    Add_Lesson_06

---
+++ Lesson 06/menu_item.py
@@ -0,0 +1,21 @@
+class MenuItem:
+    def __init__(self, name, price, description, is_vegetarian):
+        self.name = name
+        self.price = price
+        self.description = description
+        self.is_vegetarian = is_vegetarian
+
+    def get_name(self):
+        return self.name
+
+    def get_price(self):
+        return self.price
+
+    def get_description(self):
+        return self.description
+
+    def is_vegetarian(self):
+        return self.is_vegetarian
+
+    def __str__(self):
+        return f"{self.name} - ${self.price}: {self.description} (Vegetarian: {self.is_vegetarian})"
