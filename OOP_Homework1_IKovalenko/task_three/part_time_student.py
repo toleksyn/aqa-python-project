81ab9441fcf7fed914374b8ce53e8aafb3d6eeed
Viktor Nesterenko <109369648+RenkoNester@users.noreply.github.com>
2023-04-26 11:22:24 +0300

    Delete OOP_Homework1_IKovalenko directory

--- OOP_Homework1_IKovalenko/task_three/part_time_student.py
+++
@@ -1,9 +0,0 @@
-
-from student import Student
-
-class PartTimeStudent(Student):
-    def __init__(self, name, last_name, age, address):
-        super().__init__(name, last_name, age, address)
-
-    def study_type(self):
-        return "I`m studying part time"
