5b2b928b6d553b61c6b0c8f2619a2d36cceeae40
Igrekonko <Igrekonko@users.noreply.github.com>
2023-05-04 20:11:10 +0300

    Update main_instance.py

--- restaurant_ordering_system_IKovalenko/main_instance.py
+++ restaurant_ordering_system_IKovalenko/main_instance.py
@@ -10,26 +10,25 @@ borscht = Entree("Borscht", 49.95, "Traditional Ukrainian Borscht", False, True)
 juice = Drink("Juice", 12.30, "Natural juices in the assortement", "Medium", True)
 
 # Create an order
-order1 = Order(1)
-order1.add_item(appetizer1)
-order1.add_item(entree1)
-order1.add_item(drink1)
+first_order = Order(1)
+first_order.add_item(mussels)
+first_order.add_item(borscht)
+first_order.add_item(juice)
 
 # Create another order
 crackers = Appetizer("Crackers", 15.50, "Cheddar Cracker Appetizer Bites", True, False)
 butter_chicken = Entree("Butter chicken", 100.10, "Grilled chicken in a luscious tomato-based spiced curry sauce", False, False)
 tea = Drink("Tea", 12.30, "Different in the assortement", "Large", False)
-order2 = Order(2)
-order2.add_item(appetizer2)
-order2.add_item(entree2)
-order2.add_item(drink2)
+second_order = Order(2)
+second_order.add_item(crackers)
+second_order.add_item(butter_chicken)
+second_order.add_item(tea)
 
 # Create a restaurant
 my_restaurant = Restaurant("Family Food")
-my_restaurant.add_order(1, order1)
-my_restaurant.add_order(2, order2)
+my_restaurant.add_order(1, first_order)
+my_restaurant.add_order(2, second_order)
 
 # Print the orders in the restaurant
-orders = my_restaurant.get_orders()
-for table_number, order in orders.items():
-    print(f"Table {table_number}: {[item.get_name() for item in order.get_items()]} - Total: ${order.get_total()}")
+my_restaurant.print_orders(1)
+my_restaurant.print_orders(2)
