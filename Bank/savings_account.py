b2a207f6b36eb741f31b58d3b119b0125406f547
RomanO-qa <65535352+RomanO-qa@users.noreply.github.com>
2023-05-13 11:43:41 +0300

    Updated withdraw function code

--- Bank/savings_account.py
+++ Bank/savings_account.py
@@ -10,8 +10,10 @@ class SavingsAccount(Account):
         super().__init__(account_number, account_holder_name, balance, interest_rate)
         self.minimum_balance = minimum_balance
 
-    def withdraw_money(self):
-        if self.balance > self.minimum_balance:
-            print("You can withdraw $", self.balance)
+    def withdraw_money(self, amount):
+        if self.balance > self.minimum_balance and (self.balance-amount) > self.minimum_balance:
+            self.balance -= amount
+            return f'You withdraw {amount}, your current balance is {self.balance}'
         else:
-            print("Your balance is less than minimum balance")
+            return f'You can not withdraw "{amount}" since balance after withdrawing "{self.balance-amount}"' \
+                   f' will be less than minimum "{self.minimum_balance}"'
