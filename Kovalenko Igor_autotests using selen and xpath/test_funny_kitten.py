5b04b6904b889960e8003550b9c60e501dbca42a
Igrekonko <Igrekonko@users.noreply.github.com>
2023-05-13 16:27:46 +0300

    Update test_funny_kitten.py

--- Kovalenko Igor_autotests using selen and xpath/test_funny_kitten.py
+++ Kovalenko Igor_autotests using selen and xpath/test_funny_kitten.py
@@ -1,6 +1,5 @@
 from selene import browser
 from selene.support import by
-from time import sleep
 
 # Open Google home page
 browser.open_url('https://www.google.com/')
@@ -9,17 +8,6 @@ browser.open_url('https://www.google.com/')
 search_input = browser.element(by.xpath("//textarea[@id='APjFqb']"))
 search_input.type('funny kitten').press_enter()
 
-# Wait for 2 seconds for the search results to load
-sleep(2)
-
 # Verify that first result link contains 'kitten' in its text
 assert 'kitten' in browser.all("//div[@id='rso']")[0]\
                     .element(by.xpath('.//h3')).text.lower()
-
-# Close the browser
-browser.quit()
-
-
-
-
-
