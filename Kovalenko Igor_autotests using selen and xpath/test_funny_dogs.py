1f493e735da8158ac98700d63ed8d0e08f199b6f
Igrekonko <Igrekonko@users.noreply.github.com>
2023-05-13 16:26:46 +0300

    Update test_funny_dogs.py

--- Kovalenko Igor_autotests using selen and xpath/test_funny_dogs.py
+++ Kovalenko Igor_autotests using selen and xpath/test_funny_dogs.py
@@ -1,7 +1,6 @@
 from selene import browser
 from selene.support import by
 from selene.support.jquery_style_selectors import s
-from time import sleep
 
 # open google home page
 browser.open_url('https://www.google.com/')
@@ -11,12 +10,8 @@ search_input = s("//textarea[@id='APjFqb']")
 search_input.type('funny dogs').press_enter()
 
 # wait for search results to load
-browser.element(by.xpath('//*[@id="rso"]/div[1]/div[1]/div[1]/a'))
-sleep(5)
+browser.element(by.xpath('//*[@id="rso"]//a/h3'))
 
 # get search result links
 result_links = browser.all(by.xpath('//*[@id="rso"]//a/h3'))
 assert len(result_links) == 9, f'Less than 9 result links are found'
-
-# close the browser
-browser.quit()
