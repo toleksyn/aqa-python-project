b4b11b74b27548e51c6be3935b48add7831dfba9
Igrekonko <Igrekonko@users.noreply.github.com>
2023-05-13 23:45:24 +0300

    Update test_selene.py

--- Kovalenko Igor_autotests using selen and xpath/test_selene.py
+++ Kovalenko Igor_autotests using selen and xpath/test_selene.py
@@ -1,7 +1,5 @@
-from selene import browser
-from selene.support import by
+from selene import browser, by, be, have
 from selene.support.jquery_style_selectors import s
-from time import sleep
 
 # Open Google home page
 browser.open_url('https://www.google.com/')
@@ -11,18 +9,11 @@ search_input = s("//textarea[@id='APjFqb']")
 search_input.type('Selene').press_enter()
 
 # Verify that the first link contains Selene in its text
-first_result_link = browser.element('//*[@id="rso"]/div[1]/div/div')
-assert 'Selene' in first_result_link.text, f" 'Selene' word was not found in the first result link"
-
-# Wait for 5 seconds for the search results to load
-sleep(5)
-
-# Click on Google logo on the search results page
-google_logo = browser.element(by.xpath('//*[@id="logo"]/img'))
-google_logo.click()
-
-# Wait for 5 seconds for the search results to load
-sleep(5)
+browser.all("//div[@id='rso']")[0].element(by.xpath('.//h3')).should(have.text('Selene'))
 
 # Verify that you navigated back to the home page and that homepage is open
-assert "Google" in browser.title(), f"You are not navigated back to the Google homepage"
+browser.element(by.xpath("//*[@id='logo']/img")).click()
+browser.element(by.xpath("//img[@alt='Google']"))
+browser.element(by.xpath("//*[@id='APjFqb']"))
+browser.element(by.xpath("//form/div[1]/div[1]/div[4]/center/input[1]"))
+browser.element(by.xpath("//form/div[1]/div[1]/div[4]/center/input[2]"))
