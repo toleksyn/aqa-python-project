39e64d4a81ed6456c3f0c709e1c9c833d11560b5
RomanO-qa <65535352+RomanO-qa@users.noreply.github.com>
2023-05-16 11:47:44 +0300

    Delete OOP_Homework1_IKovalenko directory

--- OOP_Homework1_IKovalenko/task_two/ice_car.py
+++
@@ -1,9 +0,0 @@
-
-from car import Car
-
-class ICECar(Car):
-    def __init__(self, model, make_year):
-        super().__init__(model, make_year, "gas")
-
-    def drive(self):
-        print("I`m driving on gas")
