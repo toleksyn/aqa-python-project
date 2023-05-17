7a5b479376a021461aa83d8be3eefbe35e77e345
MDonyk <donyk.rita.testfort@gmail.com>
2023-05-18 00:26:41 +0300

    Added selene task

---
+++ donyk_selene_task/selene_test.py
@@ -0,0 +1,15 @@
+from selene import browser, by, be, have
+
+
+# Task 3
+browser.open_url("https://google.com")
+
+search_box_input = browser.element(by.xpath("//textarea[@name='q']")).should(be.blank)
+search_box_input.type('Selene').press_enter()
+
+browser.all("//div[@id='rso']")[0].element(by.xpath('.//h3')).should(have.text('Selene'))
+
+browser.element(by.xpath("//*[@id='logo']/img")).click()
+browser.element(by.xpath("//img[@alt='Google']"))
+browser.element(by.xpath("//form/div[1]/div[1]/div[4]/center/input[1]"))
+browser.element(by.xpath("//form/div[1]/div[1]/div[4]/center/input[2]"))
