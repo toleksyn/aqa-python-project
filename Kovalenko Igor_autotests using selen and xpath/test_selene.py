24f3b29833fc5880a986342bb520589274e73c96
Igrekonko <Igrekonko@users.noreply.github.com>
2023-05-21 18:23:29 +0300

    Update test_selene.py

--- Kovalenko Igor_autotests using selen and xpath/test_selene.py
+++ Kovalenko Igor_autotests using selen and xpath/test_selene.py
@@ -1,8 +1,9 @@
 from selene import browser, by, be, have
 from selene.support.jquery_style_selectors import s
+from selene import config
 
 # Open Google home page
-browser.open_url('https://www.google.com/')
+browser.open_url("https://google.com/ncr")
 
 # Search for 'Selene'
 search_input = s("//textarea[@id='APjFqb']")
@@ -13,7 +14,6 @@ browser.all("//div[@id='rso']")[0].element(by.xpath('.//h3')).should(have.text('
 
 # Verify that you navigated back to the home page and that homepage is open
 browser.element(by.xpath("//*[@id='logo']/img")).click()
-browser.element(by.xpath("//img[@alt='Google']"))
-browser.element(by.xpath("//*[@id='APjFqb']"))
-browser.element(by.xpath('//input[@value="Google Search"]'))
-browser.element(by.xpath('//input[@value="I`m Feeling Lucky"]'))
+browser.element(by.xpath("//*[@id='APjFqb']")).click()
+browser.element(by.xpath('//form/div[1]/div[1]/div[4]/center/input[1]')).click()
+browser.element(by.xpath('//form/div[1]/div[1]/div[4]/center/input[2]')).click()
