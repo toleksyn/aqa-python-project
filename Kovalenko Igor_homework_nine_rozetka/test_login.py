0969d2df8ad7205c8017c9c1df687e614aa2cada
Igrekonko <Igrekonko@users.noreply.github.com>
2023-05-21 18:32:19 +0300

    Update test_login.py

--- Kovalenko Igor_homework_nine_rozetka/test_login.py
+++ Kovalenko Igor_homework_nine_rozetka/test_login.py
@@ -10,8 +10,8 @@ browser.element(by.xpath('//button[@class="header__button ng-star-inserted"]')).
 # Verify that the login modal is displayed
 browser.element(by.xpath('//div[@class="modal__content"]')).should(be.visible)
 browser.element(by.xpath('//input[@type="email"]')).should(be.clickable)
-browser.element(by.xpath('//button[text()="Увійти"]'))
-browser.element(by.xpath('//button[text()="Зареєструватися"]'))
+browser.element(by.xpath('//button[@class="button button--large button--green auth-modal__submit ng-star-inserted"]')).should(be.clickable)
+browser.element(by.xpath('//button[@class="auth-modal__register-link button button--link ng-star-inserted"]')).should(be.clickable)
 browser.element(by.xpath('//app-slider/div/button[1]')).should(be.clickable)
 browser.element(by.xpath('//app-slider/div/button[2]')).should(be.clickable)
 
