753f1350a1718ba1b2d9f2898f4649c9a5516538
MDonyk <130687987+MDonyk@users.noreply.github.com>
2023-05-21 16:13:58 +0300

    Update main_bank.py
    Grouped 'from' statements

--- bank/main_bank.py
+++ bank/main_bank.py
@@ -1,14 +1,15 @@
 from savings_account import SavingsAccount
+from checking_account import CheckingAccount
+from bank import Bank
+
 savings_account = SavingsAccount("001", "Jack Smith", 3000, 0.1, 500)
 savings_account.withdraw(3000)
 print(savings_account.balance)
 
-from checking_account import CheckingAccount
 checking_account = CheckingAccount("002", "Lia Wu", 5000, 0.07, 1000)
 checking_account.withdraw(500)
 print(checking_account.calculate_interest())
 
-from bank import Bank
 bank = Bank()
 bank.add_account(savings_account)
 bank.add_account(checking_account)
