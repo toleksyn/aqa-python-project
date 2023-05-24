925494ae9837a1ae59accb29d2243b307af78369
Igrekonko <Igrekonko@users.noreply.github.com>
2023-05-25 01:23:02 +0300

    Update test_dell_po.py

--- tests_page_object_pattern/test_dell_po.py
+++ tests_page_object_pattern/test_dell_po.py
@@ -9,20 +9,20 @@ from basket_modal import BasketModal
 
 def test_rozetka_search_po():
     config.timeout = 40
-    # Open http://rozetka.com.ua home page
+# Open http://rozetka.com.ua home page
     home_page = RozetkaHomePage()
     home_page.open()
-    # Search for ‘dell’
+# Search for ‘dell’
     results_page = home_page.search("Dell")
-    # Verify that at least 10 result links are displayed
+# Verify that at least 10 result links are displayed
     results_page.verify_search_results_at_least(10)
-    # Save the name and the price of 5th product, add it to the basket
+# Save the name and the price of 5th product, add it to the basket
     pdp_results_page = ProductsOpen()
     product_name_plp = pdp_results_page.verify_product_name_present(product_number=5)
     product_price_plp = pdp_results_page.verify_product_price_present_on_pdp(product_number=5)
     add_product_to_basket = RozetkaSearchResultsPage()
     add_product_to_basket.add_products_to_basket(product_number=5)
-    # Verify the basket modal is opened and the price and name are correct
+ # Verify the basket modal is opened and the price and name are correct
     basket_modal = BasketModal()
     basket_modal.verify_basket_modal_title()
     basket_modal.verify_close_modal_button()
