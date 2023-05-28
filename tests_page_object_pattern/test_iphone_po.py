6017bfc4b5a6811b715db357f58c3b5116ecf8f8
Igrekonko <Igrekonko@users.noreply.github.com>
2023-05-28 17:19:09 +0300

    Update test_iphone_po.py

--- tests_page_object_pattern/test_iphone_po.py
+++ tests_page_object_pattern/test_iphone_po.py
@@ -8,7 +8,6 @@ from rozetka_search_results_page import RozetkaSearchResultsPage
 
 def test_rozetka_search_po():
     config.timeout = 40
-<<<<<<< HEAD
 
     home_page = RozetkaHomePage()
     home_page.open()
@@ -25,18 +24,3 @@ def test_rozetka_search_po():
     price_on_product_page = product_results_page.verify_product_price_present_on_pdp(product_number=3)
 
     assert product_price_on_search_page == price_on_product_page, f'The stored price and price on product page is not equal'
-=======
-# Open http://rozetka.com.ua home page
-    home_page = RozetkaHomePage()
-    home_page.open()
-# Search for ‘iPhone’
-    results_page = home_page.search("iPhone")
-    # Verify that at least 5 result links are displayed and that each link text contains 'iPhone'
-    results_page.verify_search_results_at_least(5)
-# Verify the 3rd product has a price, save the price
-    price_plp = results_page.verify_product_price_present(product_number=3)
-# Click on the 3rd product and verify that the stored price and price on product page is equal
-    pdp_results_page = ProductsOpen()
-    price_pdp = pdp_results_page.verify_product_price_present_on_pdp(product_number=3)
-    assert price_plp == price_pdp, f'The stored price and price on product page is not equal'
->>>>>>> cbee3601fb52556f945a2c01abc81c55a0a2857e
