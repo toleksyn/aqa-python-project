c547dce99eecdc480c3316a9e65442bb21d41887
Igrekonko <Igrekonko@users.noreply.github.com>
2023-05-28 17:20:00 +0300

    Update product_details_page.py

--- tests_page_object_pattern/product_details_page.py
+++ tests_page_object_pattern/product_details_page.py
@@ -6,6 +6,5 @@ class ProductDetailsPage:
     def verify_product_price_present_on_pdp(self, product_number):
         price_on_product_page = browser.element(by.xpath('//p[@class="product-price__big product-price__big-color-red"]'))
 
-
     def verify_product_name_present(self, product_number):
         name_on_product_page = browser.element(by.xpath("(//div[contains(@class,'goods-tile__inner')])//a[contains(@class,'goods-tile__heading')]/span"))
