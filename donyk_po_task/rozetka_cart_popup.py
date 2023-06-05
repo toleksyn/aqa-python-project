c3fca528b0b23d35d2b80adcc421e25dff9c7aac
MDonyk <donyk.rita.testfort@gmail.com>
2023-06-05 23:59:52 +0300

    Updated rozetka po task

--- donyk_po_task/rozetka_cart_popup.py
+++ donyk_po_task/rozetka_cart_popup.py
@@ -3,8 +3,9 @@ from selene import browser, by
 
 class CartPopup:
 
-    def save_product_name(self):
-        return browser.element(by.xpath('//a[@class="cart-product__title"]')).text
+    def get_product_name(self, product_number):
+        return browser.element(by.xpath('(//a[@class="cart-product__title"])[{0}]'.format(str(product_number)))).text
 
-    def save_product_price(self):
-        return browser.element(by.xpath('//p[@class="cart-product__price"]')).text
+    def get_product_price(self, product_number):
+        return browser.element(by.xpath('(//p[@class="cart-product__price cart-product__price--red"])[{0}]'.format(str(product_number)))).text
+
--- donyk_po_task/rozetka_home_page.py
+++ donyk_po_task/rozetka_home_page.py
@@ -1,20 +1,21 @@
 from selene import browser, by, be
-from rozetka_search_results_page import SearchResultsPage
-from rozetka_login_popup import LoginPopup
 
 
 class HomePage:
 
     def open(self):
         browser.open_url('http://rozetka.com.ua')
+        return self
 
     def search(self, keyword):
         browser.element(by.xpath('//input[@name="search"]')).type(keyword).press_enter()
+        from rozetka_search_results_page import SearchResultsPage
         return SearchResultsPage()
 
-    def open_login_popup(self):
+    def open_login(self):
         browser.element(by.xpath('//button[@class="header__button ng-star-inserted"]')).click()
+        from rozetka_login_popup import LoginPopup
         return LoginPopup()
 
-    def verify_login_popup_closed(self):
+    def verify_closed(self):
         browser.element(by.xpath('//div[@class="modal__content"]')).should_not(be.visible)
