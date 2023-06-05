5f63be614eba0c47a76da235dae8bf1248fe6f7f
MDonyk <130687987+MDonyk@users.noreply.github.com>
2023-06-06 00:14:17 +0300

    Update po_iphone_test.py

--- donyk_po_task/po_iphone_test.py
+++ donyk_po_task/po_iphone_test.py
@@ -1,13 +1,12 @@
 from rozetka_home_page import HomePage
 
-home_page = HomePage()
-home_page.open()
+home_page = HomePage().open()
 
 search_results_page = home_page.search('iPhone')
-search_results_page.verify_search_results_at_least(5)
+search_results_page.verify_size_at_least(5)
 
-third_product_price = search_results_page.verify_product_price(3)
+third_product_price = search_results_page.get_product_price(3)
 product_page = search_results_page.open_product_page(3)
-product_page_price = product_page.verify_product_price_on_product_page(3)
+product_page_price = product_page.get_product_price(3)
 
 assert third_product_price == product_page_price
