a7ba873ccdf060ccaf8e7b9554c0d27527672c0a
MDonyk <130687987+MDonyk@users.noreply.github.com>
2023-06-06 00:17:20 +0300

    Update rozetka_product_page.py

--- donyk_po_task/rozetka_product_page.py
+++ donyk_po_task/rozetka_product_page.py
@@ -3,5 +3,5 @@ from selene import browser, by
 
 class ProductPage:
 
-    def verify_product_price_on_product_page(self):
-        return browser.element(by.xpath('//p[@class="product-price__big product-price__big-color-red"]')).text
+    def get_product_price(self, product_number):
+        return browser.element(by.xpath('(//p[@class="product-price__big product-price__big-color-red"])[{0}]'.format(str(product_number)))).text
