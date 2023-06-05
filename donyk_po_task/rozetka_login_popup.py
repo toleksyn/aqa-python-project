b2b13848b3e183789f1c8a6d23c4e8a5ee63f746
MDonyk <130687987+MDonyk@users.noreply.github.com>
2023-06-06 00:16:56 +0300

    Update rozetka_login_popup.py

--- donyk_po_task/rozetka_login_popup.py
+++ donyk_po_task/rozetka_login_popup.py
@@ -1,10 +1,9 @@
 from selene import browser, by, be
-from rozetka_home_page import HomePage
 
 
 class LoginPopup:
 
-    def verify_login_popup_opened(self):
+    def verify_opened(self):
         browser.element(by.xpath('//div[@class="modal__content"]')).should(be.visible)
         browser.element(by.xpath('//input[@id="auth_email"]')).should(be.visible)
         browser.element(by.xpath('//input[@id="auth_pass"]')).should(be.visible)
@@ -12,6 +11,7 @@ class LoginPopup:
             '//button[@class="button button--large button--green auth-modal__submit ng-star-inserted"]')).should(
             be.clickable)
 
-    def close_popup(self):
+    def close(self):
         browser.element(by.xpath('//button[@class="modal__close"]')).click()
+        from rozetka_home_page import HomePage
         return HomePage()
