9241692ef4f73111581f44e9990fdcc61c13a6fa
Igrekonko <Igrekonko@users.noreply.github.com>
2023-06-08 21:01:09 +0300

    Update test_login_po.py

--- tests_page_object_pattern/test_login_po.py
+++ tests_page_object_pattern/test_login_po.py
@@ -1,14 +1,12 @@
 from selene import browser, by, be, have, config
 
 from rozetka_home_page import RozetkaHomePage
-from rozetka_home_page import LoginModal
-from login_modal import LoginModalOpened
 
 def test_rozetka_search_po():
     config.timeout = 40
 
     home_page = RozetkaHomePage().open()
-    login_modal = LoginModal().verify_opening_login_modal()
-    login_modal_opened_state = LoginModalOpened().verify_that_login_modal_opened()
-    login_modal_close = LoginModalOpened().close_modal()
-    login_modal_closed_state = LoginModal().verify_that_modal_closed()
+    login_modal = home_page.open_login_modal()
+    login_modal.verify_that_login_modal_opened()
+    home_page = login_modal.close_modal()
+    home_page.verify_that_modal_closed()
