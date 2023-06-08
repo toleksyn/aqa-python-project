39b5db9bdb16dcd5ba04b783fa65f9fa2471d51f
Igrekonko <Igrekonko@users.noreply.github.com>
2023-06-08 20:58:27 +0300

    Update rozetka_home_page.py

--- tests_page_object_pattern/rozetka_home_page.py
+++ tests_page_object_pattern/rozetka_home_page.py
@@ -1,7 +1,7 @@
 from selene import browser, by, be, have
 
 from rozetka_search_results_page import RozetkaSearchResultsPage
-from login_modal import LoginModalOpened
+from login_modal import LoginModal
 
 class RozetkaHomePage:
 
@@ -16,13 +16,10 @@ class RozetkaHomePage:
             .press_enter()
         return RozetkaSearchResultsPage()
 
-class LoginModal:
-
-    def verify_opening_login_modal(self):
+    def open_login_modal(self) -> LoginModal:
         browser.element(by.xpath('//button[@class="header__button ng-star-inserted"]')).click()
-        return self
+        return LoginModal()
 
     def verify_that_modal_closed(self):
         browser.element(by.xpath('//div[@class="modal__content"]')).should_not(be.visible)
         from rozetka_home_page import RozetkaHomePage
-        return RozetkaHomePage()
