1ebd3975b5abc04aa553c24cda3129a1b32bf2ea
MDonyk <130687987+MDonyk@users.noreply.github.com>
2023-06-11 14:21:39 +0300

    Update po_iphone_test.py

--- donyk_po_task/po_iphone_test.py
+++ donyk_po_task/po_iphone_test.py
@@ -5,8 +5,10 @@ home_page = HomePage().open()
 search_results_page = home_page.search('iPhone')
 search_results_page.verify_size_at_least(5)
 
-third_product_price = search_results_page.get_product_price(3)
-product_page = search_results_page.open_product_page(3)
-product_page_price = product_page.get_product_price(3)
+number_of_product = 3
 
-assert third_product_price == product_page_price
+third_product_price = search_results_page.get_product_price(number_of_product)
+product_page = search_results_page.open_product_page(number_of_product)
+product_page_price = product_page.get_product_price(number_of_product)
+
+assert third_product_price == product_page_price, f'Product prices are different'
