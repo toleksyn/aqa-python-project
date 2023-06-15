79308d6689b96bf28077fad2f8de89d63ad9441d
MDonyk <130687987+MDonyk@users.noreply.github.com>
2023-06-15 23:31:38 +0300

    Update po_dell_test.py

--- donyk_po_task/po_dell_test.py
+++ donyk_po_task/po_dell_test.py
@@ -7,7 +7,7 @@ search_results_page.verify_size_at_least(10)
 
 fifth_product_name = search_results_page.get_product_name(5)
 fifth_product_price = search_results_page.get_product_price(5)
-add_to_basket_button = search_results_page.add_product_to_cart(5)
+add_fifth_product_to_cart = search_results_page.add_to_cart(5)
 
 cart_popup = search_results_page.open_cart_popup()
 added_product_name = cart_popup.get_product_name(1)
