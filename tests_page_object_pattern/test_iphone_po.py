51d71b39d533bded8b9685cb3e500d23be0cb6b6
Igrekonko <Igrekonko@users.noreply.github.com>
2023-06-18 17:18:49 +0300

    Update test_iphone_po.py

--- tests_page_object_pattern/test_iphone_po.py
+++ tests_page_object_pattern/test_iphone_po.py
@@ -2,13 +2,10 @@ from selene import browser, by, be, have, config
 
 from rozetka_home_page import RozetkaHomePage
 
-def test_rozetka_search_po():
-    config.timeout = 40
-
-    home_page = RozetkaHomePage().open()
-    search_results = home_page.search("iPhone")
-    search_results.verify_search_results_at_least(5)
-    third_product_price_on_search_page = search_results.get_product_price(3)
-    third_product_details_page = search_results.open_product_details_page(3)
-    price_on_product_details_page = third_product_details_page.get_discounted_product_price()
-    assert third_product_price_on_search_page == price_on_product_details_page, f'The stored price and price on product page is not equal'
+home_page = RozetkaHomePage().open()
+search_results = home_page.search("iPhone")
+search_results.verify_search_results_at_least(5)
+third_product_price_on_search_page = search_results.get_product_price(3)
+third_product_details_page = search_results.open_product_details_page(3)
+price_on_product_details_page = third_product_details_page.get_discounted_product_price()
+assert third_product_price_on_search_page == price_on_product_details_page, f'The stored price and price on product page is not equal'
