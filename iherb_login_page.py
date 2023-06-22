bf251df4d093458cc686c0b2182dd4e9683396a2
Viktor Nesterenko <109369648+RenkoNester@users.noreply.github.com>
2023-06-22 08:44:28 +0300

    Update iherb_login_page.py

--- iherb_login_page.py
+++ iherb_login_page.py
@@ -2,13 +2,12 @@ from selene import browser, by, be
 
 class IherbLoginPage:
     def set_username(self, username):
-        browser.element(by.xpath('//input[@name="Username"]')).type(username)
+        browser.element(by.xpath('//input[@name="Username"]')).should(be.visible).type(username)
         return self
 
     def set_password(self, password):
-        browser.element(by.xpath('//input[@type="password"]')).type(password).press_enter()
+        browser.element(by.xpath('//input[@type="password"]')).should(be.visible).type(password).press_enter()
         return self
 
     def verify_that_error_message_is_displayed(self, error_text):
         browser.element(by.xpath(f'(//li[contains(text(),"{error_text}")])')).should(be.visible)
-
