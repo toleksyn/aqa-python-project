8607e4a973794357ca4ba71ee86e9fe4f9d69460
Viktor Nesterenko <109369648+RenkoNester@users.noreply.github.com>
2023-06-22 08:43:00 +0300

    Update test_iherb_task_3.py

--- test_iherb_task_3.py
+++ test_iherb_task_3.py
@@ -1,25 +1,18 @@
 from pages.iherb_home_page import IherbHomePage
-from pages.iherb_login_page import IherbLoginPage
 
 
 def test_iherb_the_third_task():
     # Open iherb home page
-    home_page = IherbHomePage()
-    home_page.open()
+    home_page = IherbHomePage().open()
 
-    # Open login page
-    home_page.open_the_login_page()
+    # # Open login page
+    login_page = home_page.open_the_login_page()
 
-    # Enter non-existing email
-    login_page = IherbLoginPage()
-    login_page.enter_login("nonexisting@mail.com")
+    # Set  non-existing username
+    login_page.set_username("nonexisting@gmail.com")
 
-    # Enter non-existing password
-    login_page.enter_password("nonexistingpassword")
+    # Set non-existing password
+    login_page.set_password("nonexistingpassword")
 
-    # Verify error message
-    actual_error_message_text = login_page.get_the_error_message_text("Invalid email, phone number, or password")
-    expected_error_message_text = "Invalid email, phone number, or password"
-
-    assert actual_error_message_text == expected_error_message_text
-
+    # Verify the error message
+    verifying_the_error_message = login_page.verify_that_error_message_is_displayed("Invalid email, phone number, or password")
