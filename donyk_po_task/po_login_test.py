c7cc3c646d425dc1b105511a1321f1e0b6da6600
MDonyk <donyk.rita.testfort@gmail.com>
2023-06-22 10:28:06 +0300

    Deleted files

--- donyk_po_task/po_login_test.py
+++
@@ -1,9 +0,0 @@
-from rozetka_home_page import HomePage
-
-home_page = HomePage().open()
-
-login_popup = home_page.open_login()
-login_popup.verify_opened()
-
-close = login_popup.close()
-close.verify_closed()
--- donyk_po_task/rozetka_product_page.py
+++
@@ -1,7 +0,0 @@
-from selene import browser, by
-
-
-class ProductPage:
-
-    def get_product_price(self, product_number):
-        return browser.element(by.xpath('(//p[@class="product-price__big product-price__big-color-red"])[{0}]'.format(str(product_number)))).text
