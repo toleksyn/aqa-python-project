3d9d0c45d394e2ad4a3e22c02b8ef073ba7bf9e2
Viktor Nesterenko <109369648+RenkoNester@users.noreply.github.com>
2023-06-29 08:17:08 +0300

    Update iherb_cart_page.py

--- iherb_cart_page.py
+++ iherb_cart_page.py
@@ -4,9 +4,8 @@ from pages.iherb_checkout_page import IherbCheckoutPage
 
 
 class IherbCartPage:
-    def verify_that_product_is_added_to_the_cart(self):
-        browser.all(by.xpath('//a[@data-qa-element="product-item-title"]')).should(be.visible)
-        return IherbCartPage()
+        def verify_that_product_is_added_to_the_cart(self):
+        browser.all(by.xpath('//a[@data-qa-element="product-item-title"]')).wait_until(be.visible)
 
     def procced_to_checkout(self):
         procced_to_checkout_button = browser.element(by.xpath('//a[@data-ga-element="btnToCheckout"]')).click()
