29e4bf80b60860dbf88fcbd2cc9fb3197d263d5c
Viktor Nesterenko <109369648+RenkoNester@users.noreply.github.com>
2023-06-29 08:21:51 +0300

    Update test_iherb_task_2.py

--- test_iherb_task_2.py
+++ test_iherb_task_2.py
@@ -1,18 +1,14 @@
 from pages.iherb_home_page import IherbHomePage
 
 def test_iherb_the_second_task():
-    # Open iherb home page
+
     home_page = IherbHomePage().open()
 
-    # Search for ‘vitamin d’
     search_result_page = home_page.search("vitamin d")
 
-    # Verify at least 5 products are displayed
     search_result_page.verify_search_results_at_least(5)
 
-    # In the filter section, check the NOW Foods checkbox
     search_result_page.set_the_filter("NOW Foods")
 
-    # Verify that all displayed products have ‘NOW Foods’ in the name
     products_include_filter_name = search_result_page.verify_that_products_include_filter_name("NOW Foods")
 
