fcaacb40825e8f8a4a956f873f131c13fc6efd8a
Viktor Nesterenko <109369648+RenkoNester@users.noreply.github.com>
2023-06-29 09:10:01 +0300

    Update iherb_checkout_page.py

--- iherb_checkout_page.py
+++ iherb_checkout_page.py
@@ -6,11 +6,11 @@ class IherbCheckoutPage:
         browser.all(f'(//div[@class="List__DisplayName-sc-1mvgpt5-4 djMfWv"]').get(query.text)
         return self
 
-    def re_enter_card_number(self, cart_number):
+    def re_enter_credit_card_number(self, cart_number):
         browser.element(by.xpath('//input[@id="encryptedCardNumber"]')).type(cart_number)
         return IherbCartPage()
 
-    def approve_cart_number(self):
+    def approve_credit_card_number(self):
         continue_button = browser.element(by.xpath('//button[@id="credit-card-continue-button"]')).click
         return IherbCartPage()
 
