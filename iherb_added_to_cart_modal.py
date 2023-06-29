0c890fc28285932faa87066e70487447415e09b5
Viktor Nesterenko <109369648+RenkoNester@users.noreply.github.com>
2023-06-29 08:15:24 +0300

    Update iherb_added_to_cart_modal.py

--- iherb_added_to_cart_modal.py
+++ iherb_added_to_cart_modal.py
@@ -3,7 +3,7 @@ from selene import browser, by
 from pages.iherb_cart_page import IherbCartPage
 
 
-class IherbAddedToCartModal:
+class IherbCartModal:
     def open_the_cart_page(self):
-        view_cart_button = browser.element(by.xpath('//div[@class="checkout-button"]')).click()
+        browser.element(by.xpath('//div[@class="checkout-button"]')).click()
         return IherbCartPage()
