2be3450f2c2a096abd0561160ac4d63826f09e15
Viktor Nesterenko <109369648+RenkoNester@users.noreply.github.com>
2023-06-29 09:08:05 +0300

    Update iherb_checkout_page.py

--- iherb_checkout_page.py
+++ iherb_checkout_page.py
@@ -1,12 +1,18 @@
 from selene import browser, by, query
-
+from pages.iherb_cart_page import IherbCartPage
 
 class IherbCheckoutPage:
-    def expand_product_panel(self):
-        browser.element(by.xpath('//div[@class="LandingPanel__ToggleText-sc-bh59r1-3 clVDdW"]')).click()
-        return IherbCheckoutPage()
-
     def get_product_name(self):
         browser.all(f'(//div[@class="List__DisplayName-sc-1mvgpt5-4 djMfWv"]').get(query.text)
         return self
 
+    def re_enter_card_number(self, cart_number):
+        browser.element(by.xpath('//input[@id="encryptedCardNumber"]')).type(cart_number)
+        return IherbCartPage()
+
+    def approve_cart_number(self):
+        continue_button = browser.element(by.xpath('//button[@id="credit-card-continue-button"]')).click
+        return IherbCartPage()
+
+    def place_oredr(self):
+        place_oredr_button = browser.element(by.xpath('//button[@id="place-order-button"]')).click
