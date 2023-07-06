5a0bc80659343dbc1cc8aa0350d6f15444849a5f
Igrekonko <Igrekonko@users.noreply.github.com>
2023-07-06 10:54:41 +0300

    Update iherb_payment_update_page.py

--- iHerb_autotests/iherb_payment_update_page.py
+++ iHerb_autotests/iherb_payment_update_page.py
@@ -4,7 +4,7 @@ from iherbtestvenv.iherb_order_details_page import IherbOrderDetailsPage
 
 class IherbPaymentUpdatePage:
 
-    def verify_displaying_order_summary_button(self):
+    def verify_that_order_summary_button_is_displayed(self):
         return browser.element(by.xpath('//div[@class="css-kvi0m9"]')).should(be.visible)
 
     def navigate_to_order_summary_page(self) -> IherbOrderDetailsPage:
