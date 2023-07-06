d7087604cd43db463a6d113ce4710c65ba6560b3
Igrekonko <Igrekonko@users.noreply.github.com>
2023-07-06 12:54:52 +0300

    Update iherb_product_details_page.py

--- iHerb_autotests/iherb_product_details_page.py
+++ iHerb_autotests/iherb_product_details_page.py
@@ -2,5 +2,5 @@ from selene import browser, by, query
 
 class IherbProductDetailsPage:
 
-    def get_product_reviews(self):
+    def get_product_reviews_amount(self):
         return browser.element(by.xpath("//div[@id='product-summary-header']//a[2]/span")).get(query.text)
