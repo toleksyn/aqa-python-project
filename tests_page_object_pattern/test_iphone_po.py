041805e09edbab9ef82294ed5b6aec6b6f0d49d0
Igrekonko <Igrekonko@users.noreply.github.com>
2023-07-06 10:01:01 +0300

    Update test_iphone_po.py

--- tests_page_object_pattern/test_iphone_po.py
+++ tests_page_object_pattern/test_iphone_po.py
@@ -6,6 +6,6 @@ home_page = RozetkaHomePage().open()
 search_results = home_page.search("iPhone")
 search_results.verify_search_results_at_least(5)
 third_product_price_on_search_page = search_results.get_product_price(3)
-third_product_details_page = search_results.open_product_details_page(3)
+third_product_details_page = search_results.open_product(3)
 price_on_product_details_page = third_product_details_page.get_discounted_product_price()
 assert third_product_price_on_search_page == price_on_product_details_page, f'The stored price and price on product page is not equal'
