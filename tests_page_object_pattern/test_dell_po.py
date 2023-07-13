16291f0ffccbd6cca4b9b2ceb355c3bf7fdf480c
Igrekonko <Igrekonko@users.noreply.github.com>
2023-07-13 22:51:55 +0300

    Update test_dell_po.py

--- tests_page_object_pattern/test_dell_po.py
+++ tests_page_object_pattern/test_dell_po.py
@@ -6,9 +6,11 @@ from rozetka_home_page import RozetkaHomePage
 home_page = RozetkaHomePage().open()
 search_results_page = home_page.search("dell")
 search_results_page.verify_search_results_at_least(10)
-fifth_product_name_on_search_page = search_results.get_product_name(5)
-fifth_product_price_on_search_page = search_results.get_product_price(5)
-add_fifth_product_to_basket = search_results.add_product_to_basket(5)
+
+product_number = 5
+fifth_product_name_on_search_page = search_results.get_product_name(product_number)
+fifth_product_price_on_search_page = search_results.get_product_price(product_number)
+add_product_to_basket = search_results.add_product_to_basket(product_number)
 basket_modal = search_results.open_basket_modal()
 product_name_in_basket = basket_modal.get_product_name(1)
 product_price_in_basket = basket_modal.get_discounted_product_price(1)
