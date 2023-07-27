36d073927bdd0a4354eddb244e148104c1abc6c6
RomanO-qa <romantestfort@gmail.com>
2023-07-27 13:17:49 +0300

    added comment to execute_script function in Homepage class

--- Omelchenko_AppiumTest/AppiumPageObjectClasses/homepage_appium.py
+++ Omelchenko_AppiumTest/AppiumPageObjectClasses/homepage_appium.py
@@ -11,5 +11,6 @@ class HomePage:
         search_field.click()
         search_box = self.driver.find_element(AppiumBy.XPATH, "//*[@text='Search iHerb']")
         search_box.send_keys(search_keyword)
+        # This needed to perform search action, because .sendKeyEvent(84) doesn't work
         self.driver.execute_script('mobile: performEditorAction', {"action": "search"})
         return SearchResultsPage(self.driver)
--- Omelchenko_AppiumTest/AppiumSelenePageObjectClasses/homepage_selene.py
+++ Omelchenko_AppiumTest/AppiumSelenePageObjectClasses/homepage_selene.py
@@ -10,5 +10,6 @@ class HomePage:
         search_field.click()
         search_box = browser.element((AppiumBy.XPATH, "//*[@text='Search iHerb']"))
         search_box.send_keys(search_keyword)
+        # This needed to perform search action, because .sendKeyEvent(84) doesn't work
         browser.execute_script('mobile: performEditorAction', {"action": "search"})
         return SearchResultsPage()
