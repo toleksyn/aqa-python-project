5f224729514de5e4ab642e4038fa68811106455d
RomanO-qa <romantestfort@gmail.com>
2023-07-28 19:05:05 +0300

    removed deprecated files

--- Omelchenko_AppiumTest/AppiumSelenePageObjectClasses/product_details_page_selene.py
+++
@@ -1,7 +0,0 @@
-from appium.webdriver.common.appiumby import AppiumBy
-from selene import browser, query
-
-
-class ProductDetailsPage:
-    def get_product_price(self):
-        return browser.element((AppiumBy.ID, 'com.iherb:id/discounted_price')).get(query.text)
