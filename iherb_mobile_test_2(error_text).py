f51f40e07ffce66e7e3acedeee068b117993e3f1
Viktor Nesterenko <109369648+RenkoNester@users.noreply.github.com>
2023-08-08 21:13:11 +0300

    Update iherb_mobile_test_2(error_text).py

--- iherb_mobile_test_2(error_text).py
+++ iherb_mobile_test_2(error_text).py
@@ -13,12 +13,11 @@ appium_server_url = 'http://localhost:4723/wd/hub'
 appium_driver = webdriver.Remote(appium_server_url, appium_options)
 appium_driver.implicitly_wait(25)
 
-
 home_screen = PreferencesScreen().save_preferences()
-my_account_screen = home_screen.my_account_icon()
-sign_in_sign_up_screen = my_account_screen.sign_in_button_1()
-create_account_screen = sign_in_sign_up_screen.sign_in_button_2()
-returning_customer_screen = create_account_screen.sign_in_button_3
+my_account_screen = home_screen.open_my_account_screen()
+sign_in_sign_up_screen = my_account_screen.open_sign_up_sign_in_account_screen()
+create_account_screen = sign_in_sign_up_screen.open_create_account_screen()
+returning_customer_screen = create_account_screen.open_returning_customer_screen
 returning_customer_screen.set_username("nonexisting@mail.com").set_password("nonexistingpassword").submit_the_login_form()
 actual_error_text = returning_customer_screen.get_error_text()
 expected_error_text = "Invalid email, phone number, or password"
