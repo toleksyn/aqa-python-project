817bb1d6d4d9c69d6bcd68d6227b41bd9c155d43
Viktor Nesterenko <109369648+RenkoNester@users.noreply.github.com>
2023-08-08 20:33:35 +0300

    Update my_account_screen.py

--- my_account_screen.py
+++ my_account_screen.py
@@ -3,9 +3,9 @@ from pages.mobile_page_objects.sign_in_sign_up_screen import SignInSignUpScreen
 from appium.webdriver.common.appiumby import AppiumBy
 
 class MyAccountScreen:
-    def __int__(self):
-        pass
+    def __init__(self, driver):
+        self.driver = driver
 
-    def sign_inbutton_1(self) -> SignInSignUpScreen:
+    def open_create_account_screen(self) -> SignInSignUpScreen:
         appium.find_element(AppiumBy.ID, 'com.iherb:id/tv_sign_in').click()
         return SignInSignUpScreen()
