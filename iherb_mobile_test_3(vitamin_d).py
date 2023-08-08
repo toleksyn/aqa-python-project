4be2479ec79576e2dabead33d01dbc12031b75d6
Viktor Nesterenko <109369648+RenkoNester@users.noreply.github.com>
2023-08-08 21:22:39 +0300

    Add files via upload

---
+++ iherb_mobile_test_3(vitamin_d).py
@@ -0,0 +1,24 @@
+from appium import webdriver
+from pages.mobile_page_objects.preferences_screen import PreferencesScreen
+
+appium_options = {
+    'platformName': 'Android',
+    'automationName': 'uiautomator2',
+    'deviceName': 'Pixel 6 PRO API 30',
+    'appPackage': 'com.iherb',
+    'appActivity': '.mobile.product.splash.view.SplashActivity'
+}
+
+appium_server_url = 'http://localhost:4723/wd/hub'
+driver = webdriver.Remote(appium_server_url, appium_options)
+driver.implicitly_wait(20)
+
+home_screen = PreferencesScreen().save_preferences()
+search_results_screen = home_screen.search('vitamin d')
+filter_name = 'NOW Foods'
+select_brand = search_results_screen.open_filter_and_sort_drawer()
+select_brand.set_filter(filter_name)
+the_first_product_name = search_results_screen.get_product_name(1)
+the_fourth_product_name = search_results_screen.get_product_name(4)
+assert the_first_product_name.startswith(filter_name), f"Product title doesn't contain {filter_name}"
+assert the_fourth_product_name.startswith(filter_name), f"Product title doesn't contain {filter_name}"
