1090d844624916a21413844a1608f022f94823f5
Viktor Nesterenko <109369648+RenkoNester@users.noreply.github.com>
2023-08-08 20:38:38 +0300

    Update sign_in_sign_up_screen.py

--- sign_in_sign_up_screen.py
+++ sign_in_sign_up_screen.py
@@ -3,10 +3,10 @@ from pages.mobile_page_objects.create_an_account import CreateAnAccountScreen
 from pages.mobile_page_objects.home_page_screen import appium_driver
 
 class SignInSignUpScreen:
-    def __init__(self):
-        pass
+    def __init__(self, driver):
+        self.driver = driver
 
-    def sign_in_button_2(self) -> CreateAnAccountScreen:
-        button2 = appium_driver.find_element(AppiumBy.ID, 'com.iherb:id/login_page_button_sign_in_with_email')
-        button2.click()
+    def open_create_account_screen(self) -> CreateAnAccountScreen:
+        sign_in_slash_sign_in_button = appium_driver.find_element(AppiumBy.ID, 'com.iherb:id/login_page_button_sign_in_with_email')
+        sign_in_slash_sign_in_button.click()
         return CreateAnAccountScreen()
